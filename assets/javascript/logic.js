e171aaf228e8cccd2b98281416b10b3835208f23
Alejandro Cortes <Alejandro Cortes>
2016-05-18 00:31:36 -0400

    trying to figure out moment.js. failing

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,7 +17,7 @@ $("#submitTrain").on("click", function() {
 
 	trainName = $('#nameinput').val().trim();
 	destination = $('#destinationinput').val().trim();
-	firstTrainTime = $('#timeinput').val().trim();
+	firstTrainTime = moment($('#timeinput').val().trim(), "HH:mm").format("HH:mm");
 	frequency = $('#frequencyinput').val().trim();
 
 
@@ -30,10 +30,17 @@ $("#submitTrain").on("click", function() {
 		dateAdded: Firebase.ServerValue.TIMESTAMP
 	})
 
+	// Hopefully clears the boxes after values submited
+	$("#trainName").val("");
+	$("#trainDest").val("");
+	$("#trainTime").val("");
+	$("#trainFreq").val("");
+
+
+
 	return false;
 
 });
-
 
 dataRef.on("child_added", function(childSnapshot) {
 	// Log everything that's coming out of snapshot
@@ -42,6 +49,14 @@ dataRef.on("child_added", function(childSnapshot) {
 	console.log(childSnapshot.val().firstTrainTime);
 	console.log(childSnapshot.val().frequency);
 	console.log(childSnapshot.val().joinDate);
+
+
+	var nextArrival = moment().diff(moment.firstTrainTime);
+	var away = moment().diff(moment.firstTrainTime);
+
+
+	// Use first arrival time and frequency to calculate the next arrival time
+	var nextArr = moment().diff(moment.unix(firstTrainTime, 'X'), "months");
 
 
 // I am not sure how this will pan out
@@ -54,25 +69,8 @@ $('.destinat').append("<div class='row'><span id='destin'> "
 $('.frequent').append("<div class='row'><span id='frequen'> "
 	+childSnapshot.val().frequency+" </span></div>")
 
-
-
-
-
-
-	// " </span><span id='destination'> "
-	// +childSnapshot.val().destination+" </span><span id='firstTrainTime'> "
-	// +childSnapshot.val().firstTrainTime+" </span><span id='frequency'> "
-	// +childSnapshot.val().frequency+" </span></div>")
-
-}, function(errorObject) {
-
+});
 });
 
-// dataRef.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-// 	$('#train-name').html(snapshot.val().trainName);
-// 	$('#destination').html(snapshot.val().destination);
-// 	$('#frequency').html(snapshot.val().frequency);
-// })
-
-});
+// console.log(nextArrival);
 
